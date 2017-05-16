aa40664cf04f07482468e96b8c88d6bd3c9710f1
spraveenitpro <praveen.selvasekaran@automattic.com>
2017-05-16 23:16:27 +0530

    Completed Menus

--- functions.php
+++ functions.php
@@ -1,83 +1,81 @@
 <?php
 /**
- * humescores functions and definitions
+ * Humescores functions and definitions.
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package humescores
+ * @package Humescores
  */
 
 if ( ! function_exists( 'humescores_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function humescores_setup() {
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on humescores, use a find and replace
-	 * to change 'humescores' to the name of your theme in all the template files.
-	 */
-	load_theme_textdomain( 'humescores', get_template_directory() . '/languages' );
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * Note that this function is hooked into the after_setup_theme hook, which
+     * runs before the init hook. The init hook is too late for some features, such
+     * as indicating support for post thumbnails.
+     */
+    function humescores_setup() {
+        /*
+         * Make theme available for translation.
+         * Translations can be filed in the /languages/ directory.
+         * If you're building a theme based on Humescores, use a find and replace
+         * to change 'humescores' to the name of your theme in all the template files.
+         */
+        load_theme_textdomain( 'humescores', get_template_directory() . '/languages' );
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support( 'automatic-feed-links' );
 
-	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
+        /*
+         * Let WordPress manage the document title.
+         * By adding theme support, we declare that this theme does not use a
+         * hard-coded <title> tag in the document head, and expect WordPress to
+         * provide it for us.
+         */
+        add_theme_support( 'title-tag' );
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-	 */
-	add_theme_support( 'post-thumbnails' );
+        /*
+         * Enable support for Post Thumbnails on posts and pages.
+         *
+         * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+         */
+        add_theme_support( 'post-thumbnails' );
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'menu-1' => esc_html__( 'Primary', 'humescores' ),
-	) );
+        // This theme uses wp_nav_menu() in one location.
+        register_nav_menus( array(
+            'primary' => esc_html__( 'Header', 'humescores' ),
+        ) );
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
+        /*
+         * Switch default core markup for search form, comment form, and comments
+         * to output valid HTML5.
+         */
+        add_theme_support( 'html5', array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+        ) );
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'humescores_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
+        // Set up the WordPress core custom background feature.
+        add_theme_support( 'custom-background', apply_filters( 'humescores_custom_background_args', array(
+            'default-color' => 'ffffff',
+            'default-image' => '',
+        ) ) );
 
-    // Add theme support for custom logo
-    add_theme_support('custom-logo', array(
-        'width' => 90,
-        'height' => 90,
-        'flex-width' => true,
+        // Add theme support for Custom Logo
+        add_theme_support( 'custom-logo', array(
+            'width' => 90,
+            'height' => 90,
+            'flex-width' => true,
+        ));
 
-    ));
-
-	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
-}
+    }
 endif;
 add_action( 'after_setup_theme', 'humescores_setup' );
+
 
 /**
  * Register custom fonts.
@@ -87,7 +85,7 @@ function humescores_fonts_url() {
 
     /**
      * Translators: If there are characters in your language that are not
-     * supported by Source Sans Pro and PT serif translate this to 'off'. Do not translate
+     * supported by Source Sans Pro and PT Serif, translate this to 'off'. Do not translate
      * into your own language.
      */
     $source_sans_pro = _x( 'on', 'Source Sans Pro font: on or off', 'humescores' );
@@ -95,19 +93,16 @@ function humescores_fonts_url() {
 
     $font_families = array();
 
-    if ('off' !== $source_sans_pro) {
-
+    if ( 'off' !== $source_sans_pro ) {
         $font_families[] = 'Source Sans Pro:400,400i,700,900';
     }
 
-    if ('off' !== $pt_serif) {
-
+    if ( 'off' !== $pt_serif ) {
         $font_families[] = 'PT Serif:400,400i,700,700i';
     }
 
-    if ( in_array( 'on', array($source_sans_pro, $pt_serif)) ) {
 
-
+    if ( in_array( 'on', array($source_sans_pro, $pt_serif) ) ) {
 
         $query_args = array(
             'family' => urlencode( implode( '|', $font_families ) ),
@@ -141,7 +136,6 @@ function humescores_resource_hints( $urls, $relation_type ) {
 }
 add_filter( 'wp_resource_hints', 'humescores_resource_hints', 10, 2 );
 
-
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
@@ -150,7 +144,7 @@ add_filter( 'wp_resource_hints', 'humescores_resource_hints', 10, 2 );
  * @global int $content_width
  */
 function humescores_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'humescores_content_width', 640 );
+    $GLOBALS['content_width'] = apply_filters( 'humescores_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'humescores_content_width', 0 );
 
@@ -160,15 +154,15 @@ add_action( 'after_setup_theme', 'humescores_content_width', 0 );
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function humescores_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'humescores' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'humescores' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+    register_sidebar( array(
+        'name'          => esc_html__( 'Sidebar', 'humescores' ),
+        'id'            => 'sidebar-1',
+        'description'   => esc_html__( 'Add widgets here.', 'humescores' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
 }
 add_action( 'widgets_init', 'humescores_widgets_init' );
 
@@ -176,18 +170,22 @@ add_action( 'widgets_init', 'humescores_widgets_init' );
  * Enqueue scripts and styles.
  */
 function humescores_scripts() {
+    // Enqueue Google Fonts: Source Sans Pro and PT Serif
+    wp_enqueue_style( 'humescores-fonts', humescores_fonts_url() );
 
-    //Enqueue Google Fonts: Source Sans and PT serif
-    wp_enqueue_style('humescores-fonts', humescores_fonts_url());
-	wp_enqueue_style( 'humescores-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'humescores-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'humescores-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+    wp_enqueue_script( 'humescores-navigation', get_template_directory_uri() . '/js/navigation.js', array('jquery'), '20151215', true );
+    wp_localize_script( 'humescores-navigation', 'humescoresScreenReaderText', array(
+        'expand' => __( 'Expand child menu', 'humescores'),
+        'collapse' => __( 'Collapse child menu', 'humescores'),
+    ));
 
-	wp_enqueue_script( 'humescores-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    wp_enqueue_script( 'humescores-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
 }
 add_action( 'wp_enqueue_scripts', 'humescores_scripts' );
 
